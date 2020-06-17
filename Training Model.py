52da0301b1bb6ccf72e8d10f13d34c82af13460c
Vishal Aggarwal <aggarwalvishal901@gmail.com>
2020-06-17 14:00:19 +0530

    Update Training Model.py

--- Training Model.py
+++ Training Model.py
@@ -1,18 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug  6 15:58:23 2018
-
-@author: Sam
-"""
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# pip install --upgrade keras
-
 
 # Importing the Keras libraries and packages
 from keras.models import Sequential
